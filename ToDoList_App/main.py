60f672f2ab92439489dda4bb613595d9a80354fa
Zakiya Whyles <zakiyawhyles@gmail.com>
2024-05-28 19:35:48 +0100

    Implementing Class models

---
+++ ToDoList_App/main.py
@@ -0,0 +1,5 @@
+# main.py
+from user_interface import start_application
+
+if __name__ == "__main__":
+    start_application()
--- ToDoList_App/views.py
+++ ToDoList_App/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
 
 # Create your views here.
+# def home(request):
+#     return render(request,'home.html')
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
